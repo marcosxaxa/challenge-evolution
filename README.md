f731be79b00f5a975e8ddeb7c27449bd50b5351a
xtenaz <marcos@xaxas.net>
2021-09-09 15:18:50 -0300

    exposing service with nodeport

--- README.md
+++ README.md
@@ -16,4 +16,16 @@ PUT GIT REPOSITORY HERE
 
 After download the code described on the last step **cd** into the folder (vagrant folder)
 
-To start the kubernetes cluster using vagrant and virtualbox enter the following path: > Test
+To start the kubernetes cluster using vagrant and virtualbox enter the following command inside the vagrant folder:
+
+> vagrant up
+
+Depending on the host resources this step may take a while. After the playbook finishes the **kubernetes** cluster along with kafka is provisioned and configured
+
+## Accessing cluster CLI
+
+First you need to ssh into the kubernetes master server:
+
+> vagrant ssh k8s-master
+
+**_ Obs: You need to be inside the vagrant folder in order to performe this step_**
---
+++ k8s-vagrant/k8s_config/scripts/deploy-script.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+masterNode=$1
+masterNodePort=$2
+
+KubeConf=--kubeconfig=/home/vagrant/.kube/config
+BasePath=/home/vagrant/k8s-app-deploy
+
+kubectl $KubeConf wait node/k8s-master --for=condition=Ready --timeout=300s
+
+kubectl $KubeConf taint node k8s-master node-role.kubernetes.io/master:NoSchedule-
+
+kubectl $KubeConf apply -f $BasePath/kafka/kafka-operator.yaml -n kafka
+
+podname=$(kubectl $KubeConf get pods -n kafka |awk -F ' ' '{print $1}' |sed -n 2p)
+
+kubectl $KubeConf wait pod/$podname --for=condition=Ready --timeout=300s -n kafka
+
+kubectl $KubeConf apply -f $BasePath/kafka/kafka-ephemeral-single.yaml -n kafka
+
+kubectl $KubeConf create ns monitoring
+
+kubectl $KubeConf apply -f $BasePath/prometheus/prometheus-additional.yaml -n monitoring
+
+kubectl $KubeConf apply -f $BasePath/prometheus/prometheus-operator.yaml -n monitoring
+
+promname=$(kubectl $KubeConf get pods -n monitoring |awk -F ' ' '{print $1}' |sed -n 2p)
+
+kubectl $KubeConf wait pod/$promname --for=condition=Ready --timeout=300s -n monitoring
+
+kubectl $KubeConf apply -f $BasePath/prometheus/ -n monitoring
+
+kubectl $KubeConf apply -f $BasePath/grafana/grafana.yaml -n monitoring
+kubectl $KubeConf apply -f $BasePath/grafana/grafana-service.yaml -n monitoring
+
+export grafanaName=$(kubectl $KubeConf get pods -n monitoring  |grep grafana | awk  '{print $1}')
+
+kubectl $KubeConf wait pod/$grafanaName --for=condition=Ready --timeout=300s -n monitoring
+
+# kubectl $KubeConf port-forward --address 0.0.0.0 $grafanaName 3000 -n monitoring &
+
+sleep 5
+
+APIKEY=$(curl -X POST -H "Content-Type: application/json" -d '{"name":"apikey", "role": "Admin"}' http://admin:admin@$masterNode:$masterNodePort/api/auth/keys |awk -F '"key":' '{print $2}' | awk -F '"' '{print $2}')
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $APIKEY" -d '{"name":"Prometheus", "type":"prometheus", "url":"http://prometheus-operated:9090", "access":"proxy", "basicAuth":false }' http://$masterNode:$masterNodePort/api/datasources
+
+cd $BasePath/grafana/dashboards
+for dash in $(ls); do 
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $APIKEY" --data-binary @$dash http://$masterNode:$masterNodePort/api/dashboards/db
+done
